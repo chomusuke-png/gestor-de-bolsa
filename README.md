94015109ef44fe7d652639e5b226e0297911f95a
chomusuke-png <owokenji@gmail.com>
2026-01-19 09:12:49 -0300

    arreglado para que sea compilable

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
-### Instalar estas librerias
-```shortcode
-pip install pandas
-pip install requests
-pip install pandas requests matplotlib
-```
-
 ### Para compilar y crear el .exe
 ```shortcode
 pip install pyinstaller
-pyinstaller --noconsole --onefile app_bolsa.py
+pyinstaller --noconsole --onefile --add-data "nombres.json;." main.py
 ```
 
 ### ¿Cómo usarlo?
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,18 @@
 import json
 import os
+import sys
 
 def cargar_configuracion(archivo_json):
-    """Carga el diccionario desde el archivo JSON si existe."""
-    if os.path.exists(archivo_json):
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+
+    ruta_completa = os.path.join(base_path, archivo_json)
+
+    if os.path.exists(ruta_completa):
         try:
-            with open(archivo_json, "r", encoding="utf-8") as f:
+            with open(ruta_completa, "r", encoding="utf-8") as f:
                 return json.load(f)
         except Exception as e:
             print(f"Error leyendo {archivo_json}: {e}")
